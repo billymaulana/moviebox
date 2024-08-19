9a434f585fff62289e8376bc087552e7ff62dc13
Billy Maulana <billymaulana1999@gmail.com>
2024-08-19 14:11:53 +0700

    chore: chore: initial commit with project setup

---
+++ README.md
@@ -0,0 +1,8 @@
+## Setup
+
+Make sure to install the dependencies:
+
+```bash
+# pnpm
+pnpm i # If you don't have pnpm installed, run: npm install -g pnpm
+```
---
+++ app/pages/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'home',
+})
+</script>
+
+<template>
+  <div>
+    <Suspense>
+      <ClientOnly>
+        Index
+      </ClientOnly>
+      <template #fallback>
+        <div italic op50>
+          <span animate-pulse>Loading...</span>
+        </div>
+      </template>
+    </Suspense>
+  </div>
+</template>
